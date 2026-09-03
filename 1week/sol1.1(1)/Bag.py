10fb1d242e2d92483534095e2af963407b481761
아루 <subil0306ab@gmail.com>
2026-09-03 10:44:54 +0900

    1주차 과제

---
+++ 1week/sol1.1(1)/Bag.py
@@ -0,0 +1,24 @@
+﻿def contains(bag,e): #bag에 e가 있는지 여부를 판별하는 함수
+    return e in bag
+
+def insert(bag,e): #새로운 항목 e를 넣는 함수
+    bag.append(e)
+
+def remove(bag,e): #항목에서 e를 삭제하는 함수
+    bag.remove(e)
+
+def count(bag): #bag에 들어 있는 항목의 수를 반환하는 함수
+    return len(bag)
+
+myBag = []
+insert(myBag,'교수님짱')
+insert(myBag,'지갑')
+insert(myBag,'오이')
+insert(myBag,'당근')
+insert(myBag,'휴대폰')
+insert(myBag,'가지')
+print('가방속의 물건',myBag)
+
+insert(myBag,'구름')
+remove(myBag,'당근')
+print('가방속의 물건',myBag)
---
+++ 1week/sol1.2(1)/fibonacci.py
@@ -0,0 +1,36 @@
+﻿import time
+
+# 순환적 구조
+
+def fib(n): 
+    if n == 0 or n==1:
+        return n
+    else:
+        return fib(n-2) + fib(n-1)
+
+#반복적 구조
+
+def fib_iter(n):
+    if n == 0 or n==1:
+        return n
+    else: return n
+    a,b = 0,1
+    i = 1
+    while i < n:
+        a,b=b ,a+b
+        i+= 1
+        return b
+
+print(f'피보나치 반복({5}) = {fib_iter(5)}')
+print(f'피보나치 순환({5}) = {fib(5)}')
+
+for i in range(1,40):
+        start = time.time()
+        fib(i)
+        time_rec = time.time() - start
+
+        start_iter = time.time()
+        fib_iter(i)
+        time_iter = time.time() - start_iter
+
+        print(f"n= {i}   반복: {time_iter:.2f} 순환: {time_rec:.2f}")
